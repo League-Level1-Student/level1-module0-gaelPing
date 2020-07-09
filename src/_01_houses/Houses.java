a7846a6726163d9a78be6ec545fd5ea66823a823
root <root@80ca9e529c4d>
2020-07-09 22:26:05 +0000

    roofs

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,10 +1,63 @@
 package _01_houses;
 
-public class Houses {
-	public static void main(String[] args) {
-		//robot rob =new
-	}
-	public void run() {
+import org.jointheleague.graphical.robot.Robot;
 
+public class Houses {
+	static Robot rob = new Robot();
+
+	public static void main(String[] args) {
+		rob.setSpeed(70);
+		rob.setX(0);
+		rob.setY(600);
+		rob.miniaturize();
+		rob.penDown();
+		
+		for (int i = 0; i < 22; i++) {
+			rob.setRandomPenColor();
+			run();
+			rob.setRandomPenColor();
+			smol();
+			rob.setRandomPenColor();
+			chonk();
+			rob.setRandomPenColor();
+		}
+
+	}
+
+	static public void run() {
+		rob.turn(90);
+		rob.move(30);
+		rob.turn(-90);
+		rob.move(60);
+		rob.turn(90);
+		rob.move(10);
+		rob.turn(90);
+		rob.move(60);
+		rob.setAngle(0);
+		
+	}
+	static public void smol() {
+		rob.turn(90);
+		rob.move(30);
+		rob.turn(-90);
+		rob.move(120);
+		rob.turn(90);
+		rob.move(10);
+		rob.turn(90);
+		rob.move(120);
+		rob.setAngle(0);
+		
+	}
+	static public void chonk() {
+		rob.turn(90);
+		rob.move(30);
+		rob.turn(-90);
+		rob.move(250);
+		rob.turn(90);
+		rob.move(10);
+		rob.turn(90);
+		rob.move(250);
+		rob.setAngle(0);
+		
 	}
 }

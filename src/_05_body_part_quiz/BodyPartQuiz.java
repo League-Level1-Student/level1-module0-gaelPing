e6a2c739f40aa17c5bccacfb68903a63eb57d1ef
patty <patty@pattys-mbp.attlocal.net>
2020-08-10 16:54:30 -0700

    help

--- src/_05_body_part_quiz/BodyPartQuiz.java
+++ src/_05_body_part_quiz/BodyPartQuiz.java
@@ -16,18 +16,20 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class BodyPartQuiz {
+	
 
 	// You can use the celebrity photos we have placed in the default java
 	// package,or if you prefer, get celebrity photos from the Internet,
 	// place them in the default package, and change the names below.
 
-	String firstImage = "src/_05_body_part_quiz/arnold.jpeg";
-	String secondImage = "src/_05_body_part_quiz/leonardo.jpeg";
-	String thirdImage = "src/_05_body_part_quiz/morgan.jpeg";
-	String fourthImage = "src/_05_body_part_quiz/jack.jpeg";
+	String firstImage = "arnold.jpeg";
+	String secondImage = "leonardo.jpeg";
+	String thirdImage = "morgan.jpeg";
+	String fourthImage = "jack.jpeg";
 
 	JFrame window = new JFrame();
 	JPanel panel = new JPanel();
+	
 
 	public void run() {
 		initializeGui();
@@ -36,6 +38,9 @@ public class BodyPartQuiz {
 
 	private void initializeGui() {
 		initializeImageList();
+		window.setSize(300, 300);
+		window.add(panel);
+		panel.add(getNextImage());
 		imageIterator = imageList.iterator();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.add(panel);
@@ -51,7 +56,7 @@ public class BodyPartQuiz {
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-
+int score=0;
 		// 2. Set the size of the window in the initializeGui() method 
 
 		// 4. Ask the user who this person is and store their answer
@@ -59,6 +64,14 @@ public class BodyPartQuiz {
 
 		// 5. Check their answer. If they guessed correctly:
 		// -- Tell them they are right and increase the score by 1
+if (guess.contains("arnold")) {
+	System.out.println("correct");
+	score++;
+	
+}
+else {
+	System.out.println("incorect that is arnold");
+}
 
 		// 6. Otherwise:
 		// -- Tell them they are wrong and who the person is

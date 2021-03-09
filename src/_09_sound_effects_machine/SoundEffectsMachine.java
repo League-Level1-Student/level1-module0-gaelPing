fe3cfb34c8a760463b711e2af1800c2c3ec8163b
student <student@ip-172-31-57-53.us-west-2.compute.internal>
2021-03-08 16:57:38 -0800

    yes

---
+++ src/_09_sound_effects_machine/SoundEffectsMachine.java
@@ -0,0 +1,31 @@
+package _09_sound_effects_machine;
+
+import java.io.File;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class SoundEffectsMachine {
+	
+	playSound("sawing-wood-daniel_simon.wav");
+
+private void playSound(String soundFile) {
+	String path = "src/_09_sound_effects_machine/";
+		File sound = new File(path+soundFile);
+		if (sound.exists()) {
+			new Thread(() -> {
+			try {
+				Clip clip = AudioSystem.getClip();
+				clip.open(AudioSystem.getAudioInputStream(sound));
+				clip.start();
+				Thread.sleep(clip.getMicrosecondLength()/1000);
+			}
+			catch (Exception e) {
+				System.out.println("Could not play this sound");
+			}}).start();
+ 		}
+		else {
+			System.out.println("File does not exist");
+		}
+	
+}}
---
+++ src/_10_cuteness_tv/CutnessTv.java
@@ -0,0 +1,42 @@
+package _10_cuteness_tv;
+
+import java.net.URI;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class CutnessTv {
+	JFrame frame = new JFrame();
+	JButton button =new JButton();
+
+	void showDucks() {
+		playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+	}
+
+	void showFrog() {
+		playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+	}
+
+	void showFluffyUnicorns() {
+		playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+	}
+
+	void playVideo(String videoID) {
+
+		// Workaround for Linux because "Desktop.getDesktop().browse()" doesn't
+		// work on some Linux implementations
+		try {
+			if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+				if (Runtime.getRuntime().exec(new String[] { "which", "xdg- open" }).getInputStream().read() != -1) {
+					Runtime.getRuntime().exec(new String[] { "xdg-open", videoID });
+				}
+			} else {
+				URI uri = new URI(videoID);
+				java.awt.Desktop.getDesktop().browse(uri);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
